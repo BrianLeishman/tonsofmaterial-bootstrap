77613f2979e227c3f744bca63ea84abb537c8f4d
Brian Leishman <brian@stumpyinc.com>
2018-05-10 22:09:47 -0400

    acheived 100% typedness with Closure Compiler

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tonsofmaterial-bootstrap
 Just another material skin for Bootstrap
+
+To compile yourself, use the Google Closure Compiler like this
+
+```bash
+google-closure-compiler --warning_level=VERBOSE --jscomp_warning=reportUnknownTypes --js tomb.js --js_output_file dist/tomb.js --compilation_level ADVANCED_OPTIMIZATIONS --externs externs/jquery-3.3.js --output_wrapper "(function() {%output%}).call(window);"
+```
--- tomb.js
+++ tomb.js
@@ -4,35 +4,48 @@
  * Created Date: Saturday April 28th 2018
  * Author: Brian Leishman
  * -----
- * Last Modified: Sat May 05 2018
+ * Last Modified: Thu May 10 2018
  * Modified By: Brian Leishman
  * -----
  * Copyright (c) 2018 Stumpyinc, LLC
  */
 
-window['Tombs'] = {};
+/**
+ * @constructor
+ * @struct
+ * @param {!jQuery} Style
+ * @param {number} Timeout
+ * @returns {!Tomb}
+ */
+function Tomb(Style, Timeout) {
+    this.Style = Style;
+    this.Timeout = Timeout;
+}
 
-$(document).on('mousedown', '.btn:not(.btn-link)', function (e) {
-    var $this = $(this);
-    var ID;
+var /** !Object<string, !Tomb> */ Tombs = {}
+
+
+$(document).on('mousedown', '.btn:not(.btn-link)', /** @this {Element} */ function (/** !jQuery.Event */ e) {
+    var /** !jQuery */ $this = $(this);
+    var /** string */ ID;
     if ($this.is('[data-tomb-id]')) {
-        ID = $this.attr('data-tomb-id');
+        ID = $this.attr('data-tomb-id').toString();
     } else {
         $this.attr({
-            'data-tomb-id': ID = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            'data-tomb-id': ID = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (/** string */ c) {
                 var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                 return v.toString(16);
             })
         });
     }
 
-    if (typeof window['Tombs'][ID] !== 'object' || window['Tombs'][ID] === null) {
-        window['Tombs'][ID] = {};
+    if (typeof Tombs[ID] === 'undefined' || !(Tombs[ID] instanceof Tomb)) {
+        Tombs[ID] = /** !Tomb */ new Tomb($(), 0);
     }
 
-    clearTimeout(window['Tombs'][ID]['Timeout']);
-    if (typeof window['Tombs'][ID]['$Style'] === 'object' && window['Tombs'][ID]['$Style'] !== null && window['Tombs'][ID]['$Style'].length !== 0) {
-        window['Tombs'][ID]['$Style'].remove();
+    clearTimeout(Tombs[ID].Timeout);
+    if (typeof Tombs[ID].Style === 'object' && Tombs[ID].Style !== null && Tombs[ID].Style.length !== 0) {
+        Tombs[ID].Style.remove();
     }
     if ($this.hasClass('ripple')) {
         $this.removeClass('ripple');
@@ -41,19 +54,19 @@ $(document).on('mousedown', '.btn:not(.btn-link)', function (e) {
         $this.addClass('ripple');
     });
 
-    window['Tombs'][ID]['$Style'] = $('<style>.btn[data-tomb-id="' + ID + '"]::before{top:' + (e.pageY - $this.offset().top).toString() + 'px;left:' + (e.pageX - $this.offset().left).toString() + 'px}</style>');
-    $('head').append(window['Tombs'][ID]['$Style']);
+    Tombs[ID].Style = $('<style>.btn[data-tomb-id="' + ID + '"]::before{top:' + (e.pageY - $this.offset().top).toString() + 'px;left:' + (e.pageX - $this.offset().left).toString() + 'px}</style>');
+    $('head').append(Tombs[ID].Style);
 
-    window['Tombs'][ID]['Timeout'] = setTimeout(function () {
+    Tombs[ID].Timeout = setTimeout(function () {
         $this.removeClass('ripple');
-        window['Tombs'][ID]['$Style'].remove();
+        Tombs[ID].Style.remove();
     }, 1000);
 });
 
-$(document).on('focus', '.form-group .form-control', function () {
+$(document).on('focus', '.form-group .form-control', /** @this {Element} */ function () {
     var $this = $(this);
     var $FormGroup = $this.closest('.form-group');
-    var $TombFormControlBorder = $FormGroup.find('.tomb-form-control-border');
+    var /** !jQuery */ $TombFormControlBorder = $FormGroup.find('.tomb-form-control-border');
     var Position = $this.position();
 
     $FormGroup.addClass('focus');
@@ -71,7 +84,7 @@ $(document).on('focus', '.form-group .form-control', function () {
     });
 });
 
-$(document).on('blur', '.form-group .form-control', function () {
+$(document).on('blur', '.form-group .form-control', /** @this {Element} */ function () {
     var $this = $(this);
     var $FormGroup = $this.closest('.form-group');
     var $TombFormControlBorder = $FormGroup.find('.tomb-form-control-border');
@@ -83,7 +96,7 @@ $(document).on('blur', '.form-group .form-control', function () {
     }
 });
 
-$(document).on('input', '.form-group .form-control', function () {
+$(document).on('input', '.form-group .form-control', /** @this {Element} */ function () {
     var $this = $(this);
     if ($this.val() === '') {
         $this.closest('.form-group').removeClass('not-empty');
@@ -91,4 +104,3 @@ $(document).on('input', '.form-group .form-control', function () {
         $this.closest('.form-group').addClass('not-empty');
     }
 });
-
